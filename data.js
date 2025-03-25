8ac16cdd53c10de6c6db02d6c1c30b66337ec968
Carlos Daniel <cdntapia@icloud.com>
2025-03-25 12:29:20 +0100

    Terminado

---
+++ data.js
@@ -0,0 +1,109 @@
+export const products = [
+    {
+      name: 'PcCom Revolt 4060 Intel Core i9-14900HX/32GB/1TB SSD/RTX 4060/17.3" + Windows 11 Home',
+      price: 1699.99,
+      stars: 4.5,
+      reviews: 147,
+      seller: 'PcComponentes',
+      image: 'https://thumb.pccomponentes.com/w-150-150/articles/1081/10813478/1930-pccom-revolt-4060-intel-core-i9-14900hx-32gb-1tb-ssd-rtx-4060-173-windows-11-home.jpg'
+    },
+
+    {
+        name: 'MSI Cyborg 15 A13VF-879XES Intel Core i7-13620H/32GB/1TB SSD/RTX 4060/15.6"',
+        price: 1098.99,
+        stars: 4.6,
+        reviews: 597,
+        seller: 'PcComponentes',
+        image: 'https://thumb.pccomponentes.com/w-150-150/articles/1081/10812222/1834-msi-cyborg-15-a13vf-879xes-intel-core-i7-13620h-32gb-1tb-ssd-rtx-4060-156-c61e902c-f640-48da-9f43-5bc595c18181.jpg'
+      },
+
+      {
+        name: 'ASUS TUF Gaming A15 FA506NCR-HN006 AMD Ryzen 7 7435HS/16GB/512GB SSD/RTX',
+        price: 789,
+        stars: 4.5,
+        reviews: 377,
+        seller: 'PcComponentes',
+        image: 'https://thumb.pccomponentes.com/w-150-150/articles/1084/10848624/1163-asus-tuf-gaming-a15-fa506ncr-hn006-amd-ryzen-7-7435hs-16gb-512gb-ssd-rtx-3050-156.jpg'
+      },
+
+      {
+        name: 'MSI Sword 16 HX B14VFKG-054XES Intel Core i9-14900HX/32GB/1TB SSD/RTX 4060/16"',
+        price: 1449,
+        stars: 4,
+        reviews: 222,
+        seller: 'PcComponentes',
+        image: 'https://thumb.pccomponentes.com/w-150-150/articles/1081/10819117/1489-msi-sword-16-hx-b14vfkg-054xes-intel-core-i9-14900hx-32gb-1tb-ssd-rtx-4060-16.jpg'
+      },
+
+      {
+        name: 'Acer Gaming Nitro V 15 ANV15-41 AMD Ryzen 7 7735HS/32GB/1TB SSD/RTX 4060/15.6"',
+        price: 949,
+        stars: 4,
+        reviews: 1297,
+        seller: 'PcComponentes',
+        image: 'https://thumb.pccomponentes.com/w-150-150/articles/1084/10840369/1500-acer-nitro-v-15-anv15-41-amd-ryzen-7-7735hs-32gb-1tb-ssd-rtx-4060-156-453aa583-2c46-432f-a8a6-02abd5494d4e.jpg'
+      },
+
+      {
+        name: 'MSI Sword 16 HX B14VGKG-878XES Intel Core i7-14650HX/32GB/1TB SSD/RTX',
+        price: 1499,
+        stars: 4,
+        reviews: 222,
+        seller: 'PcComponentes',
+        image: 'https://thumb.pccomponentes.com/w-150-150/articles/1085/10858071/1112-msi-sword-16-hx-b14vgkg-878xes-intel-core-i7-14650hx-32gb-1tb-ssd-rtx-4070-16.jpg'
+      },
+
+      {
+        name: 'MSI Thin 15 B12UC-1839XES Intel Core i7-12650H/16GB/1TB SSD/RTX 3050/15.6"',
+        price: 799,
+        stars: 4,
+        reviews: 524,
+        seller: 'PcComponentes',
+        image: 'https://thumb.pccomponentes.com/w-150-150/articles/1084/10846498/1331-msi-thin-15-b12uc-1839xes-intel-core-i7-12650h-16gb-1tb-ssd-rtx-3050-156.jpg'
+      },
+
+      {
+        name: 'MSI Cyborg 15 A13VE-472XES Intel Core i7-13620H/16GB/1TB SSD/RTX 4050/15.6"',
+        price: 898.99,
+        stars: 4.5,
+        reviews: 597,
+        seller: 'PcComponentes',
+        image: 'https://thumb.pccomponentes.com/w-150-150/articles/1075/10753244/1670-msi-cyborg-15-a13ve-472xes-intel-core-i7-13620h-16gb-1tb-ssd-rtx-4050-156.jpg'
+      },
+
+      {
+        name: 'Lenovo LOQ 15ARP9 AMD Ryzen 7 7435HS/24GB/1TB SSD/RTX 4050/15.6"',
+        price: 849,
+        stars: 4,
+        reviews: 75,
+        seller: 'PcComponentes',
+        image: 'https://thumb.pccomponentes.com/w-150-150/articles/1087/10871332/1765-lenovo-loq-15arp9-amd-ryzen-7-7435hs-24gb-1tb-ssd-rtx-4050-156.jpg'
+      },
+
+      {
+        name: 'Lenovo LOQ Gen 7 15ARP9 AMD Ryzen 7 7435HS/24GB/1TB SSD/RTX 4060/15.6"',
+        price: 949,
+        stars: 4,
+        reviews: 75,
+        seller: 'PcComponentes',
+        image: 'https://thumb.pccomponentes.com/w-150-150/articles/1084/10847197/1844-lenovo-loq-gen-7-15arp9-amd-ryzen-7-7435hs-24gb-1tb-ssd-rtx-4060-156.jpg'
+      },
+
+      {
+        name: 'HP Victus Gaming 16-r0037ns Intel Core i7-13620H/32GB/1TB SSD/RTX 4070/16.1"',
+        price: 1269,
+        stars: 4,
+        reviews: 1289,
+        seller: 'PcComponentes',
+        image: 'https://thumb.pccomponentes.com/w-150-150/articles/1082/10828808/1711-hp-victus-gaming-16-r0037ns-intel-core-i7-13620h-32gb-1tb-ssd-rtx-4070-161.jpg'
+      },
+
+      {
+        name: 'HP Victus 15-fa0058ns Intel Core i7-12650H/16GB/512GB SSD/RTX 3050/15.6"',
+        price: 769,
+        stars: 4,
+        reviews: 359,
+        seller: 'PcComponentes',
+        image: 'https://thumb.pccomponentes.com/w-150-150/articles/1082/10828806/1349-hp-victus-15-fa0058ns-intel-core-i7-12650h-16gb-512gb-ssd-rtx-3050-156.jpg'
+      },
+  ];
